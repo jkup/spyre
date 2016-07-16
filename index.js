07e261cb9ff6527479b511776d862c1e975325da
Jon Kuperman <jon.kuperman@gmail.com>
2016-07-16 14:36:55 -0700

    moved index to lib

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
-'use strict';
+/**
+ * Export Spyre
+ */
 
-// Load Modules
-const fs = require('fs');
-const marked = require('marked');
-const glob = require('glob');
-
-// Make sure both posts/ and site/ exist
-try {
-    fs.accessSync(__dirname + '/examples/posts', fs.R_OK);
-    fs.accessSync(__dirname + '/examples/site', fs.R_OK | fs.W_OK);
-}
-catch (e) {
-    console.error('Error. Please make sure posts/ and site/ exist and can be written to.');
-}
-
-// Glob over posts getting all .md files
-glob('posts/*.md', (er, files) => {
-    files.forEach((file) => {
-        const newName = file.replace('posts', 'site').replace('md', 'html');
-
-        // Read in the Markdown and return HTML
-        fs.readFile(file, 'utf8', (err, data) => {
-           if (err) throw err;
-
-            // Write HTML to file
-            fs.writeFile(newName, marked(data), (err) => {
-               if (err) throw err;
-            });
-        });
-
-    });
-});
+module.exports = require('./lib');
